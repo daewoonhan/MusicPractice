5d0eb480422622589368b42ddd66023d9ab456ef
daewoonhan <gkseodns1106@gmail.com>
2024-11-30 00:14:53 +0900

    찐막

--- MusicPractice/user/urls.py
+++ MusicPractice/user/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('login/', UserLoginView.as_view()),
     path('signup/', UserRegisterView.as_view()),
     path('profile/', UserUpdateView.as_view()),
-    path('user/', UserProfileView.as_view()),
+    path('profilecheck/', UserProfileView.as_view()),
     path('logout/', LogoutView.as_view()),
     path('refresh/', TokenRefreshView.as_view())
 ]
